fbe695d365026a866b11df75f6650974501fcd81
BIJU kUMAR <biju.official@gmail.com>
2022-01-15 16:56:48 +0530

    Added initial project files

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config( );
+
+import express from 'express';
+
+
+// const HOST = '127.0.0.1`';
+const PORT = process.env.PORT  || 3000;
+
+const server = express( );
+server.use( express.json() );
+
+server.get(`/`, (req, res) => {
+    res.send(`Welcome to our latest API's!!!`);
+});
+
+// server.all( `*`, (req, res) => {
+//     res.status( 200 ).send( `Welcome to our API's` );
+// });
+
+server.listen( PORT, ( ) => {
+    console.log(`Server running at ${PORT}...`);
+});
