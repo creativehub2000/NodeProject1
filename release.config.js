f4c7737d62eb8539c9bc25843a60a7c9e58474aa
freelearn2001 <96811704+freelearn2001@users.noreply.github.com>
2022-01-19 13:35:09 +0530

    New CI changes added

--- release.config.js
+++ release.config.js
@@ -2,8 +2,12 @@ module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/creativehub2000/NodeProject1",
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github'
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/github", {
+            assets: [
+                {path: "build.zip", label: "Build"}
+            ]
+        }]
     ]
 };
